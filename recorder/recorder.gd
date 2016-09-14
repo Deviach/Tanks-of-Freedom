00a2cd19047887e9a1c613ca3a277e383399c649
CzlowiekImadlo <quak@tlen.pl>
2016-09-14 18:05:32 +0200

    Added more controls

--- recorder/recorder.gd
+++ recorder/recorder.gd
@@ -3,7 +3,10 @@ extends Control
 
 export var initial_position = Vector2(0, 0)
 export var final_position = Vector2(0, 0)
-export var speed = 1
+export var speed_x = 1
+export var speed_y = 1
+export var ease_off = false
+export var ease_mod = 1
 
 const CAMERA_THRESHOLD = 1
 
@@ -18,16 +21,33 @@ func _process(delta):
     var delta_x
     var delta_y
     var new_position
+    var diff_x
+    var diff_y
     var position = self.camera.get_offset()
+    var movement_diff_x
+    var movement_diff_y
 
-    delta_x = abs(self.final_position.x - position.x)
-    delta_y = abs(self.final_position.y - position.x)
+    delta_x = self.final_position.x - position.x
+    delta_y = self.final_position.y - position.x
+    diff_x = abs(delta_x)
+    diff_y = abs(delta_y)
 
     print(delta_x, delta_y)
 
-    if delta_x < self.CAMERA_THRESHOLD and delta_y < self.CAMERA_THRESHOLD:
+    if diff_x < self.CAMERA_THRESHOLD and diff_y < self.CAMERA_THRESHOLD:
         return
 
-    new_position = position + Vector2(self.speed, self.speed) * delta
+    movement_diff_x = self.speed_x * delta
+    movement_diff_y = self.speed_y * delta
+    if self.ease_off:
+        delta_x = delta_x * delta * self.ease_mod
+        if abs(delta_x) > abs(movement_diff_x):
+            delta_x = movement_diff_x
+        delta_y = delta_y * delta * self.ease_mod
+        if abs(delta_y) > abs(movement_diff_y):
+            delta_y = movement_diff_y
+        new_position = position + Vector2(delta_x, delta_y)
+    else:
+        new_position = position + Vector2(movement_diff_x, movement_diff_y)
 
     self.camera.set_offset(new_position)
